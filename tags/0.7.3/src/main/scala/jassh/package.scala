3d55cbc574d724b3f9df785acc71c34081c9ead5
crosson.david@gmail.com <crosson.david@gmail.com@7d09d822-e49e-fb5b-7cdf-bcd8ab603ba1>
2012-04-12 06:47:58 +0000

    0.7.3  - JCSH updated to release 0.1.47  - SSHOptions now contains an extra field "name" which allow user to friendly identify a remote ssh system  - SSHOptions password type is now of SSHPassword type instead of String.    Implicit conversions is provided from String, Option[String]  - SSHShell batch method renamed to execute

---
+++ tags/0.7.3/src/main/scala/jassh/package.scala
@@ -0,0 +1,6 @@
+package object jassh {
+  type SSH = fr.janalyse.ssh.SSH
+  val SSH = fr.janalyse.ssh.SSH
+  type SSHOptions = fr.janalyse.ssh.SSHOptions
+  val SSHOptions = fr.janalyse.ssh.SSHOptions
+}

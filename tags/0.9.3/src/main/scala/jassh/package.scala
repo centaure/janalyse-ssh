7610145c1b25510651d92c7edeb9c1caaa1b1385
crosson.david@gmail.com <crosson.david@gmail.com@7d09d822-e49e-fb5b-7cdf-bcd8ab603ba1>
2012-07-04 19:21:17 +0000

    rollback

--- tags/0.9.3/src/main/scala/jassh/package.scala
+++
@@ -1,9 +0,0 @@
-package object jassh {
-  type SSH = fr.janalyse.ssh.SSH
-  val SSH = fr.janalyse.ssh.SSH
-  type SSHOptions = fr.janalyse.ssh.SSHOptions
-  val SSHOptions = fr.janalyse.ssh.SSHOptions
-  //implicit def stringToCommand(cmd: String) = new fr.janalyse.ssh.SSHCommand(cmd)
-  //implicit def stringListToBatchList(cmdList: Iterable[String]) = new fr.janalyse.ssh.SSHBatch(cmdList)
-  //implicit def stringToRemoteFile(filename: String) = new fr.janalyse.ssh.SSHRemoteFile(filename)
-}

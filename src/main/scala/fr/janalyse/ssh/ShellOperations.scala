655fa5359e6be7a8bc2913e7f11d50d86e0bc569
crosson.david <crosson.david@gmail.com>
2013-06-14 15:10:25 +0000

    lastModified added but to be finished and tested

--- src/main/scala/fr/janalyse/ssh/ShellOperations.scala
+++ src/main/scala/fr/janalyse/ssh/ShellOperations.scala
@@ -20,8 +20,7 @@ import com.typesafe.scalalogging.slf4j.Logging
 import java.text.SimpleDateFormat
 import java.util.Date
 import scala.collection.generic.CanBuildFrom
-
-
+import java.util.Locale
 
 /**
  * ShellOperations defines generic shell operations and common shell commands shortcuts
@@ -40,7 +39,7 @@ trait ShellOperations extends CommonOperations with Logging {
    * @param cmd command to be executed
    * @return A tuple made of the result string and the exit code
    */
-  def executeWithStatus(cmd: SSHCommand): Tuple2[String,Int]
+  def executeWithStatus(cmd: SSHCommand): Tuple2[String, Int]
 
   /**
    * Execute the current batch (list of commands) and return the result as a string collection
@@ -60,7 +59,6 @@ trait ShellOperations extends CommonOperations with Logging {
     builder.result
   }
 
-  
   /**
    * Execute the current command and pass the result to the given code
    * @param cmd command to be executed
@@ -105,16 +103,51 @@ trait ShellOperations extends CommonOperations with Logging {
     genoptcmd(s"""ls -ld "$filename" """).map(_.split("""\s+""")(4).toLong)
 
   /**
+   * Remote file last modified date (TZ is taken into account)
+   * @param filename file name
+   * @return optional date, or None if filename was not found
+   */
+
+  def lastModified(filename: String): Option[Date] = {
+    osid match {
+      case Linux =>
+        // 2013-02-27 18:08:51.252312190 +0100
+        val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S Z")
+        /*
+        genoptcmd(s"""ls -ld --full-time "$filename" """)
+          .map { line =>
+            val strdate = line.split("""\s+""")
+              .drop(5)
+              .take(3)
+              .mkString(" ")
+            fullTimeSDF.parse(strdate)
+          }
+          */
+        genoptcmd(s"""stat -c '%y' '$filename' """).map { sdf.parse(_) }
+        
+      case AIX =>
+        // Last modified:  Fri Apr 30 09:10:22 DFT 2010
+        val sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.US)
+        genoptcmd(s"""istat '$filename' | grep "Last modified" """)
+          .map {_.split(":",2).drop(1).mkString.trim }
+          .map {_.replaceFirst("DFT","CET")} // Because DFT is a specific AIX TZ naming for CET !!!
+          .map { sdf.parse(_) }
+
+      case _ => ???
+    }
+  }
+
+  /**
    * Remote tree file size in kilobytes
    * @param filename file name
    * @return optional file tree size in kilobytes, or None if filename was not found
    */
- def du(filename:String): Option[Long] = {
+  def du(filename: String): Option[Long] = {
     genoptcmd(s"""du -k "$filename" | tail -1""")
-        .flatMap(_.split("""\s+""",2).headOption)
-        .map(_.toLong)
+      .flatMap(_.split("""\s+""", 2).headOption)
+      .map(_.toLong)
   }
-    
+
   /**
    * Remote file md5sum
    * @param filename file name
@@ -122,7 +155,7 @@ trait ShellOperations extends CommonOperations with Logging {
    */
   def md5sum(filename: String): Option[String] = {
     osid match {
-      case Darwin => genoptcmd(s"""md5 "$filename" """).map(_.split("=",2)(1).trim)
+      case Darwin => genoptcmd(s"""md5 "$filename" """).map(_.split("=", 2)(1).trim)
       case AIX    => genoptcmd(s"""csum -h MD5 "$filename" """).map(_.split("""\s+""")(0).trim)
       case _      => genoptcmd(s"""md5sum "$filename" """).map(_.split("""\s+""")(0).trim)
     }
@@ -139,7 +172,6 @@ trait ShellOperations extends CommonOperations with Logging {
       case AIX    => genoptcmd(s"""csum -h SHA1 "$filename" """).map(_.split("""\s+""")(0).trim)
       case _      => genoptcmd(s"""sha1sum "$filename" """).map(_.split("""\s+""")(0))
     }
-    
 
   /**
    * who am I ?
@@ -169,19 +201,19 @@ trait ShellOperations extends CommonOperations with Logging {
     case "darwin" => Darwin
     case "sunos"  => SunOS
   }
-  
+
   /**
-   * remote environment variables 
+   * remote environment variables
    * @return map of environment variables
    */
-  def env: Map[String,String] = {
+  def env: Map[String, String] = {
     for {
       line <- execute("env").split("""\n""")
       EnvRE(key, value) <- EnvRE.findFirstIn(line)
-      } yield {key -> value}
+    } yield { key -> value }
   }.toMap
-  
-  private val EnvRE="""([^=]+)=(.*)""".r
+
+  private val EnvRE = """([^=]+)=(.*)""".r
 
   /**
    * List files in specified directory
@@ -196,7 +228,7 @@ trait ShellOperations extends CommonOperations with Logging {
    */
   def ls(dirname: String): Iterable[String] = {
     //executeAndTrimSplit("""ls --format=single-column "%s" """.format(dirname))
-    executeAndTrimSplit("""ls "%s" | cat """.format(dirname)).filter(_.size>0)
+    executeAndTrimSplit("""ls "%s" | cat """.format(dirname)).filter(_.size > 0)
   }
 
   /**
@@ -216,7 +248,7 @@ trait ShellOperations extends CommonOperations with Logging {
    * Of course this requires a persistent shell session to be really useful...
    * @param dirname directory name
    */
-  def cd(dirname: String) {execute(s"""cd "$dirname" """)}
+  def cd(dirname: String) { execute(s"""cd "$dirname" """) }
 
   /**
    * Get remote host name
@@ -232,7 +264,7 @@ trait ShellOperations extends CommonOperations with Logging {
     val d = executeAndTrim("date -u '+%Y-%m-%d %H:%M:%S %Z'")
     sdf.parse(d)
   }
-  private lazy val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z") 
+  private lazy val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z")
 
   /**
    * Get the content of a file
@@ -259,8 +291,8 @@ trait ShellOperations extends CommonOperations with Logging {
     //deprecated : // def ellapsedInMn(thatDate:Date):Long =  (new Date().getTime - thatDate.getTime)/1000/60
     val findpattern = osid match {
       case Linux | AIX => """find %s -follow -type f -mmin '-%d' 2>/dev/null""" // "%s" => %s to enable file/dir patterns
-      case SunOS => throw new RuntimeException("SunOS not supported - find command doesn't support -mmin parameter")
-      case _ => """find %s -type f -mmin '-%d' 2>/dev/null"""
+      case SunOS       => throw new RuntimeException("SunOS not supported - find command doesn't support -mmin parameter")
+      case _           => """find %s -type f -mmin '-%d' 2>/dev/null"""
     }
     val findcommand = findpattern.format(root, ellapsedInMn(after))
     executeAndTrimSplit(findcommand)
@@ -317,96 +349,92 @@ trait ShellOperations extends CommonOperations with Logging {
    */
   def options: SSHOptions
 
-  
   /**
    * list active processes of unix like systems
    * @return system processes list
    */
   def ps(): List[Process] = {
-    def processLinesToMap(pscmd:String, format:String):List[Map[String,String]] = {
-        val fields = format.split(",")
-        executeAndTrimSplit(pscmd)
-            .toList
-            .tail     // Removing header line
-            .map(_.trim)
-            .map(_.split("""\s+""", fields.size))
-            .filter(_.size == fields.size)
-            .map(fields zip _)
-            .map(_.toMap)
+    def processLinesToMap(pscmd: String, format: String): List[Map[String, String]] = {
+      val fields = format.split(",")
+      executeAndTrimSplit(pscmd)
+        .toList
+        .tail // Removing header line
+        .map(_.trim)
+        .map(_.split("""\s+""", fields.size))
+        .filter(_.size == fields.size)
+        .map(fields zip _)
+        .map(_.toMap)
     }
     osid match {
       case Linux =>
         val format = "pid,ppid,user,stat,vsz,rss,etime,cputime,cmd"
         val cmd = s"ps -eo $format | grep -v grep | cat"
-          
-        processLinesToMap(cmd,format).map { m =>
+
+        processLinesToMap(cmd, format).map { m =>
           LinuxProcess(
-              pid     = m("pid").toInt,
-              ppid    = m("ppid").toInt,
-              user    = m("user"),
-              state   = LinuxProcessState.fromSpec(m("stat")),
-              rss     = m("rss").toInt,
-              vsz     = m("vsz").toInt,
-              etime   = ProcessTime(m("etime")),
-              cputime = ProcessTime(m("cputime")),
-              cmdline = m("cmd")
+            pid = m("pid").toInt,
+            ppid = m("ppid").toInt,
+            user = m("user"),
+            state = LinuxProcessState.fromSpec(m("stat")),
+            rss = m("rss").toInt,
+            vsz = m("vsz").toInt,
+            etime = ProcessTime(m("etime")),
+            cputime = ProcessTime(m("cputime")),
+            cmdline = m("cmd")
           )
         }
       case AIX =>
         val format = "pid,ppid,ruser,args"
         val cmd = s"ps -eo $format | grep -v grep | cat"
-        processLinesToMap(cmd,format).map { m =>
+        processLinesToMap(cmd, format).map { m =>
           AIXProcess(
-              pid     = m("pid").toInt,
-              ppid    = m("ppid").toInt,
-              user    = m("ruser"),
-              cmdline = m("args")
+            pid = m("pid").toInt,
+            ppid = m("ppid").toInt,
+            user = m("ruser"),
+            cmdline = m("args")
           )
         }
       case SunOS =>
         val format = "pid,ppid,ruser,args"
         val cmd = s"ps -eo $format | grep -v grep | cat"
-        processLinesToMap(cmd,format).map { m =>
+        processLinesToMap(cmd, format).map { m =>
           SunOSProcess(
-              pid     = m("pid").toInt,
-              ppid    = m("ppid").toInt,
-              user    = m("ruser"),
-              cmdline = m("args")
+            pid = m("pid").toInt,
+            ppid = m("ppid").toInt,
+            user = m("ruser"),
+            cmdline = m("args")
           )
         }
       case Darwin =>
         val format = "pid,ppid,user,state,vsz,rss,etime,cputime,args"
         val cmd = s"ps -eo $format | grep -v grep | cat"
-        processLinesToMap(cmd,format).map { m =>
+        processLinesToMap(cmd, format).map { m =>
           DarwinProcess(
-             pid     = m("pid").toInt,
-              ppid    = m("ppid").toInt,
-              user    = m("user"),
-              state   = DarwinProcessState.fromSpec(m("state")),
-              rss     = m("rss").toInt,
-              vsz     = m("vsz").toInt,
-              etime   = ProcessTime(m("etime")),
-              cputime = ProcessTime(m("cputime")),
-              cmdline = m("args")
+            pid = m("pid").toInt,
+            ppid = m("ppid").toInt,
+            user = m("user"),
+            state = DarwinProcessState.fromSpec(m("state")),
+            rss = m("rss").toInt,
+            vsz = m("vsz").toInt,
+            etime = ProcessTime(m("etime")),
+            cputime = ProcessTime(m("cputime")),
+            cmdline = m("args")
 
           )
-        }        
+        }
       case x =>
         logger.error(s"Unsupported operating system $x for ps method")
         List.empty[Process]
     }
   }
-  
 
-
-  
   /**
    * File system remaining space in MB
    * @return fs freespace in Mb if the path is valid
    */
-  def fsFreeSpace(path:String):Option[Double] = {
+  def fsFreeSpace(path: String): Option[Double] = {
     osid match {
-      case Linux|AIX|Darwin => 
+      case Linux | AIX | Darwin =>
         executeAndTrimSplit(s"""df -Pm '${path}'""").drop(1).headOption.flatMap { line =>
           line.split("""\s+""").toList.drop(3).headOption.map(_.toDouble)
         }
@@ -420,59 +448,58 @@ trait ShellOperations extends CommonOperations with Logging {
    * get file rights string (such as 'drwxr-xr-x')
    * @return rights string
    */
-  def fileRights(path:String):Option[String] = {
+  def fileRights(path: String): Option[String] = {
     osid match {
-      case Linux => 
+      case Linux =>
         executeAndTrim(s"test '${path}' && stat --format '%A' '${path}'") match {
           case "" => None
           case x  => Some(x)
         }
-      case AIX|Darwin   => 
+      case AIX | Darwin =>
         executeAndTrim(s"test '${path}' && ls -lad '${path}'") match {
           case "" => None
-          case x  => x.split("""\s+""",2).headOption
+          case x  => x.split("""\s+""", 2).headOption
         }
       case x =>
         logger.error(s"Unsupported operating system $x for fileRights method")
         None
     }
   }
-  
+
   /**
    * kill specified processes
    */
-  def kill(pids:Iterable[Int]) { execute(s"""kill -9 ${pids.mkString(" ")}""") }
+  def kill(pids: Iterable[Int]) { execute(s"""kill -9 ${pids.mkString(" ")}""") }
 
   /**
    * delete files
    */
-  def rm(files:Iterable[String]) { execute(s"""rm -f ${files.mkString("'","' '", "'")}""") }
-  
+  def rm(files: Iterable[String]) { execute(s"""rm -f ${files.mkString("'", "' '", "'")}""") }
+
   /**
    * delete directory (directory must be empty
    */
-  def rmdir(dirs:Iterable[String]) { execute(s"""rmdir ${dirs.mkString("'","' '", "'")}""") }
-  
+  def rmdir(dirs: Iterable[String]) { execute(s"""rmdir ${dirs.mkString("'", "' '", "'")}""") }
+
   /**
    * get server architecture string
    * @return server architecture
    */
   def arch = execute("arch")
-  
-  /**
-   * Create a new directory 
-   */
-  def mkdir(dirname:String) {execute(s"""mkdir -p '$dirname'""")}
 
+  /**
+   * Create a new directory
+   */
+  def mkdir(dirname: String) { execute(s"""mkdir -p '$dirname'""") }
 
   // ==========================================================================================
-  
+
   /**
    * internal helper method
    */
   private def genoptcmd(cmd: String): Option[String] = {
     executeAndTrim("""%s 2>/dev/null""".format(cmd)) match {
-      case "" => None
+      case ""  => None
       case str => Some(str)
     }
   }
